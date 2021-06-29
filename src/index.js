73f5d06f77e744846c52908d4c3fa96dc4746531
phartenfeller <pharti@hotmail.de>
2021-06-29 10:15:15 +0200

    initial demo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'aframe';
 import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
@@ -8,14 +9,40 @@ class MyComponent extends LitElement {
 
   static get styles() {
     return css`
-      .my-class {
-        color: red;
+      .frame {
+        height: 500px;
+        width: 900px;
+        border: solid black;
       }
     `;
   }
 
+  /* Do not create shadow dom */
+  createRenderRoot() {
+    return this;
+  }
+
   render() {
-    return html`<span class="my-class">${this.text}</span>`;
+    return html`<div class="frame">
+      <a-scene>
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-cylinder
+          position="1 0.75 -3"
+          radius="0.5"
+          height="1.5"
+          color="#FFC65D"
+        ></a-cylinder>
+        <a-plane
+          position="0 0 -4"
+          rotation="-90 0 0"
+          width="4"
+          height="4"
+          color="#7BC8A4"
+        ></a-plane>
+        <a-sky color="#ECECEC"></a-sky>
+      </a-scene>
+    </div>`;
   }
 }
 
