c4e636a2dc2dbaa77805349ec12aff3044b69e8c
phartenfeller <pharti@hotmail.de>
2021-06-29 12:08:38 +0200

    build optimization

--- webpack.demo.js
+++ webpack.demo.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -30,13 +31,29 @@ module.exports = {
       filename: 'index.html',
       template: 'index.html',
     }),
+    new RemovePlugin({
+      before: {
+        test: [
+          {
+            folder: './demo',
+            method: (absoluteItemPath) =>
+              new RegExp(/(\.js|\.txt)$/, 'm').test(absoluteItemPath),
+            recursive: true,
+          },
+        ],
+      },
+    }),
   ],
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({ test: /\.min\.js$/ })],
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
+    },
   },
   output: {
     path: path.resolve(__dirname, 'demo'),
-    filename: 'index.js',
+    filename: '[name].js',
   },
 };
